b4f9e91b94a137709b0ccce93d5ec896cd2a08ee
Aakash Verma <info.aakash11@gmail.com>
2025-05-31 12:07:25 +0530

    updated search functionality

--- react/ecommerce-react/src/components/ProductList.jsx
+++ react/ecommerce-react/src/components/ProductList.jsx
@@ -1,17 +1,19 @@
-import ProductCard from "./ProductCard";
+import ProductCard from './ProductCard';
 // import products from '../data/products';
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 const ProductList = () => {
   let [productList, setProductList] = useState([]);
-  let [searchText, setSearchText] = useState("");
+  const [filteredProductList, setFilteredProductList] = useState(productList);
+  let [searchText, setSearchText] = useState('');
 
   const fetchData = async () => {
     const response = await fetch(
-      "https://682755e76b7628c5290ff8b1.mockapi.io/api/v1/products/products"
+      'https://682755e76b7628c5290ff8b1.mockapi.io/api/v1/products/products'
     );
     const products = await response.json();
     setProductList(products);
+    setFilteredProductList(products);
   };
 
   useEffect(() => {
@@ -19,29 +21,51 @@ const ProductList = () => {
   }, []);
 
   useEffect(() => {
-    console.log("Hello");
+    console.log('Hello');
   }, []);
 
   function handleFilterButtonClick() {
-    productList = productList.filter((product) => product.rating > 4);
-    setProductList(productList);
+    const filteredProducts = productList.filter(
+      (product) => product.rating > 4
+    );
+    setFilteredProductList(filteredProducts);
+  }
+
+  function handleSearchFilter() {
+    const filteredProducts = productList.filter((product) =>
+      product.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+    setFilteredProductList(filteredProducts);
+  }
+
+  function handleResetButton() {
+    setFilteredProductList(productList);
   }
 
   return (
-    <section className="products">
+    <section className='products'>
       <h1>Trending Products</h1>
-      {/* 
-        div className=search-filter
-            search input : onChange((e) => setSearchText(e.target.value))
-            button with name Search: onClick={handleSearchFilter}
 
-        div
-      */}
-      <button onClick={handleFilterButtonClick} className="filter-button">
-        Filter Top Rated Products
-      </button>
-      <div className="products-grid">
-        {productList.map((product) => (
+      <div className='search-filter'>
+        <input
+          className='search-filter-input'
+          type='search'
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <button className='search-button' onClick={handleSearchFilter}>
+          Search
+        </button>
+        <button onClick={handleFilterButtonClick} className='filter-button'>
+          Filter Top Rated Products
+        </button>
+        <button className='reset-button' onClick={handleResetButton}>
+          Reset
+        </button>
+      </div>
+
+      <div className='products-grid'>
+        {filteredProductList.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
