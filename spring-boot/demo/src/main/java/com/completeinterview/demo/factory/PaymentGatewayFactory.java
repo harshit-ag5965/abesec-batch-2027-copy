220cfd3e09d2f2cd2dd02c690f04588aa2653347
Aakash Verma <info.aakash11@gmail.com>
2025-08-03 11:33:14 +0530

    Paytm Added

--- spring-boot/demo/src/main/java/com/completeinterview/demo/factory/PaymentGatewayFactory.java
+++ spring-boot/demo/src/main/java/com/completeinterview/demo/factory/PaymentGatewayFactory.java
@@ -2,6 +2,7 @@ package com.completeinterview.demo.factory;
 
 import com.completeinterview.demo.service.GooglePay;
 import com.completeinterview.demo.service.PaymentGateway;
+import com.completeinterview.demo.service.Paytm;
 import com.completeinterview.demo.service.PhonePe;
 
 public class PaymentGatewayFactory {
@@ -10,8 +11,12 @@ public class PaymentGatewayFactory {
             return new PhonePe();
         } else if (mode.equals("gpay")) {
             return new GooglePay();
+        } else if (mode.equals("paytm")) {
+            return new Paytm();
         } else {
             return null;
         }
     }
 }
+
+// O: Open for extension, closed for modification
