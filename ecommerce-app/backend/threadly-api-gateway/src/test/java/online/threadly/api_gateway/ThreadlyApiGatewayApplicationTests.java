25cdd441459f1d74af60f4a4a423e1f796a7c83e
Aakash Verma <info.aakash11@gmail.com>
2025-09-29 21:05:31 +0530

    API Gateway Completed - Sept 29

---
+++ ecommerce-app/backend/threadly-api-gateway/src/test/java/online/threadly/api_gateway/ThreadlyApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package online.threadly.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadlyApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
